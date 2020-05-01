d207cd6a3a7fe49ee7aba8ec8c88d1582649d33d
Vinicius Caetano <49661832+ViniciusHooud@users.noreply.github.com>
2020-05-01 16:38:18 -0300

    trabalho mobile

---
+++ MobileCamoes/MobileCamoes/Infra/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace MobileCamoes.Infra
+{
+    public class AppSettings
+    {
+        public const string ApiUrl = "https://api.themoviedb.org/3";
+        public const string ApiKey = "5e24c8d754be7a8e2ed368324ca41272";
+        public const string ApiImageBaseUrl = "https://image.tmdb.org/t/p/w500/";
+
+        public static string OfflineDataBaseLiteDBPath { get; set; }
+        public static string OfflineDataBaseSQLitePath { get; set; }
+        public static string OfflineFileSystemPath { get; set; }
+    }
+}
